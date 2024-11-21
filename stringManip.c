f9d660493c35561d21c5c4adb5cb4a8c38af1fed
NicholasKeenan <nicholas.keenan@maine.edu>
2024-11-21 12:02:37 -0500

    str_cut function

--- stringManip.c
+++ stringManip.c
@@ -9,15 +9,11 @@ char* str_trim ( char* s ) { // Trim function
 
     if ( s == NULL || *s == '\0' ) return NULL;
 
-    while ( isspace ( *s ) ) { // Looks at beginning
-        s++;
-    }
+    while ( isspace ( *s ) ) s++; // Looks at beginning
 
     char *end = s + strlen ( s ) - 1; // Pointer to end
 
-    while ( isspace ( *end ) ) { // Trims trailing space
-        end--;
-    }
+    while ( isspace ( *end ) ) end--; // Trims trailing space
 
     *( end + 1 ) = 0; // Sets new NULL terminator
 
@@ -25,13 +21,20 @@ char* str_trim ( char* s ) { // Trim function
 
 }
 
-char *str_cut ( char * s, int start, int end ) { // Cut function
+char *str_cut ( char* s, int start, int end ) { // Cut function
 
-    char *e = s + end - 1; //pointer to end of string
-    *e = 0;
+    // char *e = s + end - 1; //pointer to end of string
+    // *e = 0;
+
+    // s += start;
+
+    // return s;
 
     s += start;
 
-    return s;
+    char buffer[2048];
+    strncpy ( buffer, s, end - start );
+
+    return buffer;
 
 }
