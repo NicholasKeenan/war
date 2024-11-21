64fea21a430fddeddba7f3aaa7c634af6b5f2647
NicholasKeenan <nicholas.keenan@maine.edu>
2024-11-21 11:48:44 -0500

    added a trim function

--- stringManip.c
+++ stringManip.c
@@ -4,3 +4,23 @@
 #include <stdlib.h>
 
 #include "stringManip.h"
+
+char* str_trim ( char* s ) { // Trim function
+
+    if ( s == NULL || *s == '\0' ) return NULL;
+
+    while ( isspace ( *s ) ) { // Looks at beginning
+        s++;
+    }
+
+    char *end = s + strlen ( s ) - 1; // Pointer to end
+
+    while ( isspace ( *end ) ) { // Trims trailing space
+        end--;
+    }
+
+    *( end + 1 ) = 0; // Sets new NULL terminator
+
+    return s;
+
+}
