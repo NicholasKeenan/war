20aa1b9267777ff36cc3f96e52c8404c409bc377
NicholasKeenan <nicholas.keenan@maine.edu>
2024-11-21 11:36:41 -0500

    CARD struct created

--- cardManip.c
+++ cardManip.c
@@ -1,2 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct { //CARD struct declaration
+    char suit[16]; //suit of the card
+    char card[16]; //the card itself ( "ace", "king", "two", etc. )
+    int value; //value of the card ( "two" = 2, etc )
+}CARD;
+
