c677ceeaa39d79eaa6cc0145fc034a49aff7fd09
NicholasKeenan <nicholas.keenan@maine.edu>
2024-12-17 05:17:11 -0500

    increased size of variables in card structure

--- cardManip.h
+++ cardManip.h
@@ -2,9 +2,9 @@
 #define CARD_MANIP_H
 
 typedef struct { //CARD struct declaration
-    char suit[16]; //suit of the card
-    char card[16]; //the card itself ( "ace", "king", "two", etc. )
-    int value[16]; //value of the card ( "two" = 2, etc )
+    char suit[2048]; //suit of the card
+    char card[2048]; //the card itself ( "ace", "king", "two", etc. )
+    int value[2048]; //value of the card ( "two" = 2, etc )
 }CARD;
 
 typedef struct {
