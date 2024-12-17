533e6456992b406e6228d2f16a2a876f5acaba4a
NicholasKeenan <nicholas.keenan@maine.edu>
2024-12-17 00:11:13 -0500

    Made it so you can choose to start or get instructions. additionally I moved most of the gameplay stuff over to gameplay.c
    Many lines to check inputs and so on

--- main.c
+++ main.c
@@ -7,28 +7,68 @@
 #include "deckManip.h"
 
 int main ( int argc, char *argv[] ) {
-    
-    int cardArraySize = 52; // Card array variable
-    int roundCounter = 0; //round counter variable
 
-    PLAYER player1, player2; //creates players
+    char choice;
 
-    CARD *cardArray = readCardFile ( argv[1], &cardArraySize ); //brings file into main
-    if ( cardArray == NULL ) return 0; //deals with errors reading file
+    //STARTUP PROCEDURE
+    while ( choice != '\n' || choice != '1' ) {
 
-    shuffleDeck ( cardArray ); //shuffles deck
-    dealCards ( cardArray, &player1, &player2 ); //deals each player their cards
+        greenText(); //makes text green
 
-    while ( player1.handSize > 0 && player2.handSize > 0 ) { //plays rounds until one player has 0 cards left
+        //Displays instructions to player on how to start or see rules of the game
+        printf ( "\nHello, Welcome!\n\nTo recieve instructions and begin the game... Just Press Enter on Your Keyboard\n\nTo Get the Rules of The Game... Just Press 1 On Your Keyboard" );
+        choice = getchar(); //collects user input
+        if ( choice == '\n' ) {//starts game if player presses enter
+            
+            printf ( "\nGreat! Let's begin.\nTo Start Each Round As Well As This One, Just Press 'ENTER' On Your Keyboard!\n\n" );
 
-        playRound ( &player1, &player2, &roundCounter );
+            choice = NULL; //resets choice
 
-    }
+            while ( choice != '\n' ) { //gets new choice
 
-    if ( player1.handSize > 0 ) { //determines game winner
-        printf ( "You Win This Game!\n" );
-    } else {
-        printf ( "You Lose... Computer Win This Game!\n" );
+                printf ( "Please Press 'ENTER' To Begin:\n" );
+                choice = getchar();
+
+            }
+
+            resetText_Color(); //sets text color back to white
+            break; //ends while loop
+
+        } else if ( choice == '1' ) { //prints instructions, rules of the game, and controls if user enters 1
+
+            choice = NULL; //resets choice
+
+            //prints the game mechanics/rules of war
+            printf ( "\nHere Are The Rules of War!\nIf At Any Time You Would Like to Start The Game, Just Press Enter on Your Keyboard.\n\n1. The deck of cards is shuffled and each player is dealt half of the total cards (26)\n\n2. Each round the players will each set down their top card to face off.\n\n3. Those cards will then be compared... The one with the highest value wins the round.\n\n4. The winner of each round will recieve the losing players card and keep their own.\n\n5. This will continue until one player has lost all of their cards and the winner will be the other player.\n\n\n" );
+            printf ( "Controls:\nTo end each round as well as start the next one, just press 'ENTER' on your keyboard.\n\n" );
+
+            while ( choice != '\n' ) { //gets new choice
+
+                printf ( "Please Press 'ENTER' To Begin:\n" );
+                choice = getchar();
+
+            }
+
+            resetText_Color(); //sets text color back to white
+            break; //ends while loop
+
+        } else {
+            
+            while ( choice != '\n' || choice != '1' ) { //checks if invalid choice has been changed
+                choice = NULL; //resets choice
+                redText();
+                printf ( "\nInvalid Choice, Enter 'ENTER' or '1'.\n" ); //tells user that the choice is invalid
+                choice = getchar();
+            }
+
+            resetText_Color(); //sets text color back to white
+
+            break; //ends while loop
+
+        }
+        resetText_Color();
+
+        break; //ends loop
     }
 
     free ( cardArray );
