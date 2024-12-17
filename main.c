1e980febb704b25ee2b682e10ada8963a8582fff
NicholasKeenan <nicholas.keenan@maine.edu>
2024-12-17 03:31:13 -0500

    changed NULL to '\0' to reset choice

--- main.c
+++ main.c
@@ -27,7 +27,7 @@ int main ( int argc, char *argv[] ) {
                 
                 printf ( "\nGreat! Let's begin.\nTo Start Each Round As Well As This One, Just Press 'ENTER' On Your Keyboard!\n\n" );
 
-                choice = NULL; //resets choice
+                choice = '\0'; //resets choice
 
                 while ( choice != '\n' ) { //gets new choice
 
@@ -42,7 +42,7 @@ int main ( int argc, char *argv[] ) {
 
             } else if ( choice == '1' ) { //prints instructions, rules of the game, and controls if user enters 1
 
-                choice = NULL; //resets choice
+                choice = '\0'; //resets choice
 
                 //prints the game mechanics/rules of war
                 printf ( "\nHere Are The Rules of War!\nIf At Any Time You Would Like to Start The Game, Just Press Enter on Your Keyboard.\n\n1. The deck of cards is shuffled and each player is dealt half of the total cards (26)\n\n2. Each round the players will each set down their top card to face off.\n\n3. Those cards will then be compared... The one with the highest value wins the round.\n\n4. The winner of each round will recieve the losing players card and keep their own.\n\n5. This will continue until one player has lost all of their cards and the winner will be the other player.\n\n\n" );
@@ -62,7 +62,7 @@ int main ( int argc, char *argv[] ) {
             } else {
                 
                 while ( choice != '\n' && choice != '1' ) { //checks if invalid choice has been changed
-                    choice = NULL; //resets choice
+                    choice = '\0'; //resets choice
                     redText();
                     printf ( "\nInvalid Choice, Enter 'ENTER' or '1'.\n" ); //tells user that the choice is invalid
                     choice = getchar();
