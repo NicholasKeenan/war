34a3c07b5b58896360ec27c470e41fe33116109e
NicholasKeenan <nicholas.keenan@maine.edu>
2024-12-17 07:40:04 -0500

    fixed segmentation fault

--- cardManip.h
+++ cardManip.h
@@ -4,11 +4,11 @@
 typedef struct { //CARD struct declaration
     char suit[2048]; //suit of the card
     char card[2048]; //the card itself ( "ace", "king", "two", etc. )
-    int value[2048]; //value of the card ( "two" = 2, etc )
+    int value; //value of the card ( "two" = 2, etc )
 }CARD;
 
 typedef struct {
-    CARD hand[26];
+    CARD hand[53];
     int handSize;
 } PLAYER;
 
