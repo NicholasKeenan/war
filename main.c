59f71869f71e7fa2fa572680ef4ecda4fd5bd8a8
NicholasKeenan <nicholas.keenan@maine.edu>
2024-12-16 22:39:15 -0500

    added function to detect a full game win and rearranged to be easier to understand

--- main.c
+++ main.c
@@ -9,22 +9,27 @@
 int main ( int argc, char *argv[] ) {
     
     int cardArraySize = 52; // Card array variable
-
-    CARD *cardArray = readCardFile ( argv[1], &cardArraySize ); //brings file into main
-
-    if ( cardArray == NULL ) return 0; //deals with errors reading file
-
-    shuffleDeck ( cardArray ); //shuffles deck
+    int roundCounter = 0; //round counter variable
 
     PLAYER player1, player2; //creates players
 
+    CARD *cardArray = readCardFile ( argv[1], &cardArraySize ); //brings file into main
+    if ( cardArray == NULL ) return 0; //deals with errors reading file
+
+    shuffleDeck ( cardArray ); //shuffles deck
     dealCards ( cardArray, &player1, &player2 ); //deals each player their cards
 
-    printf ( "Player 1's Hand:\n" );
-    printCard ( player1.hand, 26 );
+    while ( player1.handSize > 0 && player2.handSize > 0 ) { //plays rounds until one player has 0 cards left
 
-    printf ( "Player 2's Hand:\n" );
-    printCard ( player2.hand, 26 );
+        playRound ( &player1, &player2, &roundCounter );
+
+    }
+
+    // printf ( "Player 1's Hand:\n" );
+    // printCard ( player1.hand, 26 );
+
+    // printf ( "Player 2's Hand:\n" );
+    // printCard ( player2.hand, 26 );
 
     free ( cardArray );
 
