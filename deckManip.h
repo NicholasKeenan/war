f179e1b131ecab4cb15ccc31633baf5cdf7c8a9b
NicholasKeenan <nicholas.keenan@maine.edu>
2024-12-16 21:14:22 -0500

    error fixing

--- deckManip.h
+++ deckManip.h
@@ -1,5 +1,5 @@
-#ifndef STRING_MANIP_H
-#define STRING_MANIP_H
+#ifndef DECK_MANIP_H
+#define DECK_MANIP_H
 
 void shuffleDeck ( CARD deck[] )
 void dealCards ( CARD deck[], PLAYER *player1, PLAYER *player2 )
