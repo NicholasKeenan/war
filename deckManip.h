d973fe65a289856f2538cc7a520bd30069f7717a
NicholasKeenan <nicholas.keenan@maine.edu>
2024-12-24 16:53:54 -0500

    added the freeMemory function to the header file

--- deckManip.h
+++ deckManip.h
@@ -4,5 +4,6 @@
 void shuffleDeck ( CARD deck[] );
 void dealCards ( CARD deck[], PLAYER *player1, PLAYER *player2 );
 void playRound ( PLAYER *player1, PLAYER *player2, int *roundCounter );
+void freeMemory ( PLAYER *player1, PLAYER *player2, CARD *cardArray );
 
 #endif
